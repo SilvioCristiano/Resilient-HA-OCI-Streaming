cbdce82c78088378a09aa1d9e47327bfcf1a7090
SilvioCristiano <sivuca1@gmail.com>
2026-07-21 15:35:39 -0300

    Fix config path and update stream OCID
    Updated the configuration path and primary stream OCID.

--- Resilient-HA-OCI-Streaming/src/main/java/com/resilient/oci/StreamProducer.java
+++ Resilient-HA-OCI-Streaming/src/main/java/com/resilient/oci/StreamProducer.java
@@ -6,13 +6,13 @@ import com.oracle.bmc.auth.ConfigFileAuthenticationDetailsProvider;
 public class StreamProducer {
 
     public static void main(String[] args) throws Exception {
-        final String configPath = "C:\\Users\\Silvio\\.oci\\config";
+        final String configPath = "C:\\Users\\.oci\\config";
         final String profile = "DEFAULT";
 
         var cfg = ConfigFileReader.parse(configPath, profile);
         var provider = new ConfigFileAuthenticationDetailsProvider(cfg);
 
-        final String PRIMARY_STREAM_OCID = "ocid1.stream.oc1.sa-saopaulo-1."; // ajusta
+        final String PRIMARY_STREAM_OCID = "ocid1.stream.oc1.sa-saopaulo-1..."; // ajusta
         final String PRIMARY_ENDPOINT = "https://cell-1.streaming.sa-saopaulo-1.oci.oraclecloud.com"; // ajusta
 
         StreamManager manager = new StreamManager(provider, PRIMARY_STREAM_OCID, PRIMARY_ENDPOINT);
